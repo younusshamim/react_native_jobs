84b0e6ab35399e4dabb30932589c5f6aeb6dce52
younusshamim <younusshamim.bd@gmail.com>
2023-05-01 18:58:58 +0600

    first commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+import {
+  View, 
+  Text,
+  TextInput, 
+  TouchableOpacity, 
+  Image, 
+  FlatList, 
+ } from 'react-native';
+import { useRouter } from 'expo-router';
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
+
+const Welcome = () => {
+  const router = useRouter();
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Younus</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View></View>
+    </View>
+  )
+}
+
+export default Welcome
